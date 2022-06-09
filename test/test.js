4e303f5a2a51c822ff9649f69259dd050e5d58e7
Minh <dackhers@gmail.com>
2022-06-10 00:36:34 +0700

    Add .env file and dotenv dependence

--- test/test.js
+++ test/test.js
@@ -5,31 +5,47 @@ let chaiHttp = require('chai-http')
 let app = require('../index')
 let should = chai.should()
 
+let credential = {
+    username: '191210709',
+    password: '85273200'
+}
+
 chai.use(chaiHttp)
 
 describe('Api', () => {
-    beforeEach((done) => {
-        //Before each test we empty the database in your case
-        done()
-    })
-    /*
-     * Test the /GET route
-     */
     describe('/POST schedule', () => {
-        it('it should get all the schedule', (done) => {
-            let credential = {
-                username: '191210709',
-                password: '85273200'
-            }
-
+        it('it should returns an object', (done) => {         
             chai.request(app)
                 .post('/schedule')
                 .send(credential)
                 .end((err, res) => {
-                    res.should.have.status(200);
-                    res.body.should.be.a('array');
+                    res.should.have.status(200)
+                    res.body.should.be.a('object')
                     done()
                 })
-        });
-    });
-});
+        }),
+        it('it should returns an array in Data property', (done) => {         
+            chai.request(app)
+                .post('/schedule')
+                .send(credential)
+                .end((err, res) => {
+                    res.should.have.status(200)
+                    res.body.should.be.a('object')
+                    res.body.should.have.property('data').be.a('array')
+                    done()
+                })
+        }),
+        it('it should returns an array of subject object in Data', (done) => {         
+            chai.request(app)
+                .post('/schedule')
+                .send(credential)
+                .end((err, res) => {
+                    res.should.have.status(200)
+                    res.body.should.be.a('object')
+                    res.body.data.should.be.a('array')
+                   
+                    done()
+                })
+        })
+    })
+})
