a5ed60ee709e1ecea2729281134ecd17c5b499a0
hminh2027 <dackheros@gmail.com>
2023-08-20 00:43:23 +0700

    update payload

--- controllers/student.js
+++ controllers/student.js
@@ -1,18 +1,24 @@
-const { getHTML } = require('../getHTML')
-const { getStudent } = require('../htmlHandler')
+const { getHTML } = require("../getHTML");
+const { getStudent } = require("../htmlHandler");
 
-module.exports.getStudent = async (req,res)=>{
-    const {username, password} = req.body
-    if (!username || !password) return res.status(400).json({data: '', error: 'Username or password not found!'})
+module.exports.getStudent = async (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password)
+    return res
+      .status(400)
+      .json({ data: "", error: "Username or password not found!" });
 
-    try {      
-        const html = await getHTML(username, password, 'StudentProfileNew/HoSoSinhVien.aspx')
-        const data = await getStudent(html)
+  try {
+    const html = await getHTML(
+      username,
+      password,
+      "StudentProfileNew/HoSoSinhVien.aspx"
+    );
+    const data = await getStudent(html);
 
-        return res.status(200).json({data, error:''})
-
-    } catch (err) {
-        console.log(err)
-        return res.status(err.status).json({data: '', error: err.body})
-    }  
-}
+    return res.status(200).json({ data, error: "" });
+  } catch (err) {
+    console.log({ err });
+    return res.status(err.status).json({ data: "", error: err.body });
+  }
+};
